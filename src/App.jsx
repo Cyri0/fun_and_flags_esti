d50d91c6fdac1c0aa3c5b832f74f4805fde9c2bb
58 - NT <you@example.com>
2026-04-28 18:44:04 +0200

    Implement country listing with filtering and dark mode toggle; remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,50 @@
+import { useEffect, useState } from "react"
+import CountryCard from "./components/CountryCard"
+
 function App() {
+  const [countries, setCountries] = useState()
+  const regions = ["All", "Africa", "Americas", "Asia", "Europe", "Oceania", "Polar"]
+  const [selectedRegion, setSelectedRegion] = useState(regions[0])
+  const [textSearch, setTextSearch] = useState("")
+
+  const [isDarkMode, setIsDarkMode] = useState((new Date()).getHours() >= 18) 
+
+  useEffect(() => {
+    fetch("data.json")
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+
+  useEffect(()=>{
+    document.body.className = isDarkMode ? "dark" : "light"
+  }, [isDarkMode])
+
+  const filterByRegion = (pre) => {
+    if (selectedRegion === "All") return pre
+
+    return pre.filter(li => li.region === selectedRegion)
+  }
+
+  const filterByName = (pre) => {
+    return pre.filter(element => element.name.includes(textSearch))
+  }
+
   return (
     <>
+      <div>
+        <input type="text" value={textSearch} onChange={(e) => setTextSearch(e.target.value)} />
+
+        <select onChange={(e) => setSelectedRegion(e.target.value)}>
+          {regions.map(region => <option value={region}>{region}</option>)}
+        </select>
+
+        <button onClick={()=>setIsDarkMode(prev => !prev)}>
+          {isDarkMode ? "🌛" : "🌞"}
+        </button>
+      </div>
+      <section className="cardWrapper">
+        {countries && filterByName(filterByRegion(countries)).map(country => <CountryCard {...country} />)}
+      </section>
     </>
   )
 }
