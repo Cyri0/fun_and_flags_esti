d66b5cad968e7fb55f82358220d0a67d92c607b0
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-05-12 18:07:45 +0200

    Implement country detail display with data fetching and loading state

--- src/DetailPage.jsx
+++ src/DetailPage.jsx
@@ -1,11 +1,40 @@
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom"
 
 const DetailPage = () => {
-  const params = useParams()
-  console.log(params);
-  
+  const params = useParams()  
+  const [country, setCountry] = useState()
+
+  useEffect(()=>{
+    fetch("/data.json")
+    .then(response => response.json())
+    .then(data => {
+        let temp = data.find(element => element.alpha3Code.toUpperCase() == params.code.toUpperCase())
+
+        if(temp){
+            setCountry(temp)
+        }else{
+            alert("Nincs ilyen ország!")
+        }
+    })
+  },[params])
+
+  if(!country) return <div>Loading...</div>
+
   return (
-    <div>DetailPage</div>
+    <div>
+        <img style={{ maxWidth: "min(500px, 80vw)"}} src={country.flag}/>
+        <h1>{country.name}</h1>
+        <h2>{country.translations.hu}</h2>
+        <div><strong>Population: </strong> {country.population}</div>
+        <div><strong>Capital: </strong> {country.capital}</div>
+        <div><strong>Area: </strong> {country.area}</div>
+        <hr />
+        <div><strong>Region: </strong> {country.region}</div>
+        <div><strong>Subregion: </strong> {country.subregion}</div>
+        <hr />
+        {country.borders?.map(border => <Link to={"/detail/" + border}>{border}</Link>)}
+    </div>
   )
 }
 
